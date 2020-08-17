3d1bad0a760987561780aa3a18081c40d35e5e10
Lynnsey Martin <lmartin@hubspot.com>
2020-08-17 18:42:53 -0400

    javascript solution for #12

--- javascript/euler-012.js
+++ javascript/euler-012.js
@@ -1,3 +1,43 @@
 'use es6';
 
-console.log(`solution: `);
+const NUM_DIVISORS = 500;
+
+const primeFactors = (num) => {
+  let factors = [];
+  let factor = 2;
+
+  while (num > 1) {
+    if (num % factor === 0) {
+      num = num / factor;
+      factors = factors.concat(factor);
+    } else {
+      factor++;
+    }
+  }
+  return factors;
+};
+
+const divisors = (num) => {
+  const factors = primeFactors(num);
+  return factors.reduce(
+    (acc, num) =>
+      acc.concat(
+        acc
+          .map((divisor) => divisor * num)
+          .filter((divisor) => acc.indexOf(divisor) < 0)
+      ),
+    [1]
+  );
+};
+const triangleNumberWithDivisors = (numDivisors) => {
+  let triangleNum = 0;
+  let diff = 1;
+
+  while (divisors(triangleNum).length <= numDivisors) {
+    triangleNum += diff;
+    diff++;
+  }
+  return triangleNum;
+};
+
+console.log(`solution: ${triangleNumberWithDivisors(NUM_DIVISORS)}`);
